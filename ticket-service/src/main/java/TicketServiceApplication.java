a6555522511bbcaca11d3454875ffde72109508e
ubuntueric <187919712@qq.com>
2025-09-08 12:48:01 +0800

    环境搭建

---
+++ ticket-service/src/main/java/TicketServiceApplication.java
@@ -0,0 +1,10 @@
+package org.example.ticket;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TicketServiceApplication.class, args);
+    }
+}
